d1c1b91e63210350a12ebea4107ed4922378751f
Brooke Heck <b19heck@gmail.com>
2022-07-25 12:00:38 -0700

    style index page

--- js/app.js
+++ js/app.js
@@ -12,8 +12,41 @@ class Product {
     this.displayCounter = 0;
     this.clickCounter = 0;
   }
+
+  createProductDisplay() {
+    let voteDiv = document.createElement('div');
+  
+    let header = document.createElement('h2');
+    header.innerHTML = this.productName;
+    voteDiv.appendChild(header);
+      
+    let img = document.createElement('img');
+    img.src = this.productPath;
+    voteDiv.appendChild(img);
+  
+    let voteButton = document.createElement('button');
+    voteButton.innerHTML = 'Vote';
+    voteButton.addEventListener('click', () => {
+      this.clickCounter++;
+      if(voteCounter < votesAllowed) {
+        voteContainer.innerHTML = "";
+        displayProducts();
+        voteCounter++;
+      } else {
+        voteContainer.innerHTML = "";
+        viewResults();
+      }
+    });
+    voteDiv.appendChild(voteButton);
+  
+    this.displayCounter++;
+    return voteDiv;
+  }
 }
 
+let voteCounter = 0;
+let votesAllowed = 5;
+let voteContainer = document.querySelector('#voteContainer');
 let productInfoArr = [
   ['nameOne', 'https://place-hold.it/300x375/ddd" alt="placeHolder'],
   ['nameTwo', 'https://place-hold.it/300x375/ddd" alt="placeHolder'],
@@ -23,7 +56,6 @@ let productInfoArr = [
 ];
 
 let productObjArr = [];
-
 function createObjects() {
   for (let i = 0; i < productInfoArr.length; i++) {
     let newProd = new Product(productInfoArr[i][0], productInfoArr[i][1]);
@@ -33,57 +65,25 @@ function createObjects() {
 createObjects();
 
 function viewResults() {
-  let resultsDiv = document.createElement('div');
+  let resultsDiv = document.createElement('ul');
   for (let productObj of productObjArr) {
-    let header = document.createElement('h2');
+    let header = document.createElement('li');
     header.innerHTML = productObj.productName;
     resultsDiv.appendChild(header);
 
-    let voted = document.createElement('p');
-    voted.innerHTML = `${productObj.productName} was displayed ${productObj.displayCounter} time${productObj.displayCounter === 1 ? '' : 's'} and you voted for it ${productObj.clickCounter} time${productObj.clickCounter === 1 ? '' : 's'}`;
+    let voted = document.createElement('li');
+    voted.innerHTML = `${productObj.productName} was displayed ${productObj.displayCounter} time${productObj.displayCounter === 1 ? '' : 's'} and you voted for it ${productObj.clickCounter} time${productObj.clickCounter === 1 ? '' : 's'}.`;
     resultsDiv.appendChild(voted);
   }
-  gridContainer.appendChild(resultsDiv);
+  voteContainer.appendChild(resultsDiv);
 }
-
-let voteCounter = 0;
-let gridContainer = document.querySelector('#voteContainer');
-
-function createProductDisplay() {
-  let gridDiv = document.createElement('div');
-  let prodIndex = Math.floor(Math.random() * productObjArr.length);
-  
-  let header = document.createElement('h2');
-  header.innerHTML = productObjArr[prodIndex].productName;
-  gridDiv.appendChild(header);
-    
-  let img = document.createElement('img');
-  img.src = productObjArr[prodIndex].productPath;
-  gridDiv.appendChild(img);
-
-  let voteButton = document.createElement('button');
-  voteButton.innerHTML = 'Vote';
-  voteButton.addEventListener('click', () => {
-    productObjArr[prodIndex].clickCounter++;
-    if(voteCounter < 4) {
-      gridContainer.innerHTML = "";
-      displayProducts();
-      voteCounter++;
-    } else {
-      gridContainer.innerHTML = "";
-      viewResults();
-    }
-  });
-  gridDiv.appendChild(voteButton);
-
-  productObjArr[prodIndex].displayCounter++;
-  return gridDiv;
-}
-
 
 function displayProducts() {
+  let indexArray = [];
+  let index = -1;
   for(let i = 0; i < 3; i++) {
-    gridContainer.appendChild(createProductDisplay());
+    index = Math.floor(Math.random() * productObjArr.length);
+    voteContainer.appendChild(productObjArr[index].createProductDisplay());
   }
 }
 displayProducts();
